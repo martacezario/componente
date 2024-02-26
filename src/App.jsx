bf216733413cce4adc618340929dd18a315bd256
Marta Cezario <martacezario85@gmail.com>
2024-02-26 14:33:03 +0000

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Header from "./componentes/Header/Header.jsx"
+
+function App (){
+  return(
+    <>
+    <h1>Hello, Teacher!</h1>
+    
+    <Header />
+    </>
+
+
+  )
+}
+
+export default App
---
+++ src/componentes/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header(){
+    return(
+        <header>
+
+            <h2>Eu sou o componente que você pediu!</h2>
+        </header>
+
+    )
+}
+
+
+
+
+
+
+export default Header;
